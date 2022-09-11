7da65decbf4420f4f053b7f305cd4c5917d56712
sammyjcb <72704147+sammy-jcb@users.noreply.github.com>
2022-09-11 19:20:05 +0000

    script to pint out sizes of various data types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ int main(void)
 		long long int d;
 		float e;
 
-		printf("Size of a char: %d byte(s)\n", sizeof(char));
-		printf("Size of an int: %d byte(s)\n", sizeof(int));
-		printf("Size of a long int: %d byte(s)\n", sizeof(long int));
-		printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
-		printf("Size of a float: %d bytes(s)\n", sizeof(float));
+		printf("Size of a char: %ld byte(s)\n", sizeof(char));
+		printf("Size of an int: %ld byte(s)\n", sizeof(int));
+		printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+		printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
+		printf("Size of a float: %ld bytes(s)\n", sizeof(float));
 		return (0);
 	}
