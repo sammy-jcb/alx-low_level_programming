d63cbf5a835c473dc4a73e793757d434b0afbfc4
sammyjcb <72704147+sammy-jcb@users.noreply.github.com>
2022-09-11 19:09:57 +0000

    script that prints to standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+include <unistd.h>
+
+/**
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ * followed by a new line, to standard error.
+ * Description: Prints out an error
+ * Return: 1 Error found
+ */
+
+int main(void)
+	{
+		write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+		return (1);
+	}
