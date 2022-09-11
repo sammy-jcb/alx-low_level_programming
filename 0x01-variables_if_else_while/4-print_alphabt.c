30b3e0cb3db3f736ef96c251571d05fa9dfd7e2c
sammyjcb <72704147+sammy-jcb@users.noreply.github.com>
2022-09-11 21:53:18 +0000

    if, else & while variable

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *main - Prints the alphabet in lowercase, except for q and e.
+ *
+ *Return: Always (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'e' && c != 'q')
+			putchar(c);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
