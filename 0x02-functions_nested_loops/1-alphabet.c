3c48023c78770c3ab20be2c0433380c5d3d2740c
sammy-jcb <72704147+sammy-jcb@users.noreply.github.com>
2022-09-16 00:42:47 +0000

    printing alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *print_alphabet in lowercase, then a new line
+ *
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+
+	_putchar ('\n');
+
+}
