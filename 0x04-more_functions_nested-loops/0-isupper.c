715f1e681df095e77e05a10990f3724f5eb8d482
sammy-jcb <72704147+sammy-jcb@users.noreply.github.com>
2022-09-16 03:08:30 +0000

    more functions and nested loops

--- 0x04-more_functions_nested-loops/0-isupper.c
+++ 0x04-more_functions_nested-loops/0-isupper.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
-* _isupper - checks if a  character is uppercase or not
+* _isupper checks if a  character is uppercase or not
 * @c: character to be tested
-* Return: 1 whether it is, 0 otherwise
+* Return: 1 if it is, 0 otherwise
 */
 
 int _isupper(int c)
