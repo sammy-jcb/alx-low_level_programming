7dd57f7f5a2ab24571b33cac51840ef0b4286e44
sammy-jcb <72704147+sammy-jcb@users.noreply.github.com>
2022-09-17 00:18:24 +0000

    fix and buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,8 +2,8 @@
 
 /**
  * main - Prints the numbers form 1-100, but for multples of three,
- * Fizz is printed instead of the number for multiple of five,
- * Buzz and for multiples of both three and five, FizzBuzz.
+ * 	Fizz is printed instead of the number for multiple of five,
+ *	Buzz and for multiples of both three and five, FizzBuzz.
  *
  * Return: Always 0
  */
@@ -11,7 +11,7 @@ int main(void)
 {
 	int num;
 
-	for (num = 1; number <= 100; num++)
+	for (num = 1; num <= 100; num++)
 	{
 		if ((num % 3) == 0 && (num % 5) == 0)
 			printf("FizzBuzz");
