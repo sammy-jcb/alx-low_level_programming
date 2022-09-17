ae32b8ee3e3ddca77a5291c0ac994970e4e276d2
sammy-jcb <72704147+sammy-jcb@users.noreply.github.com>
2022-09-17 00:48:56 +0000

    adding 100.files

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,8 +1,8 @@
-include <stdio.h>
+#include <stdio.h>
 
 /**
  * main - Finds and prints the largest prime
- * 	  factor of the number 61285275143
+ * factor of the number 61285275143
  *
  * Return: Always 0
  */
